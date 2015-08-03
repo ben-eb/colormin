fef15e8ca1b96895f480005d8cef5f39c4147744
Ben Briggs <beneb.info@gmail.com>
2015-08-03 20:19:21 +0100

    Fixes #2 (node 0.10 compatibility issue).

--- src/lib/toLonghand.js
+++ src/lib/toLonghand.js
@@ -2,6 +2,8 @@
 
 export default hex => {
     let h = hex.substring(1);
-    let [r, g, b] = h;
+    let r = h[0];
+    let g = h[1];
+    let b = h[2];
     return h.length === 3 && '#' + r + r + g + g + b + b || hex;
 };
